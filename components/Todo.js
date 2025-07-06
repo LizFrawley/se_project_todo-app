e37676097f3c1256eee7ddf00ae87b003c60c3a7
Elizabeth Frawley <efrawley529@icloud.com>
2025-07-06 14:46:38 -0400

    Implement Todo class for managing todo items and update index.js to utilize the class for todo generation

---
+++ components/Todo.js
@@ -0,0 +1,46 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._todoTemplate = document.querySelector(selector);
+  }
+
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  getView() {
+    this._todoElement = this._todoTemplate.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    todoDate.textContent = this._data.date;
+
+    this._generateCheckboxEl();
+    this._setEventListeners();
+
+    return this._todoElement;
+  }
+}
+
+export default Todo;
