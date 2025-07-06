ac7e110cdf85e0774762a9a3b385cfce15c65f50
Elizabeth Frawley <efrawley529@icloud.com>
2025-07-06 17:46:41 -0400

    Refactor FormValidator and validation functions to improve input validation and button state management

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,14 +1,59 @@
+import { checkInputValidity, toggleButtonState } from "../scripts/validate.js";
+
 class FormValidator {
   constructor(settings, formElement) {
     this._formElement = formElement;
-    this._inputSelector = settings.inputSelector;
-    this._submitButtonSelector = settings.submitButtonSelector;
-    this._errorClass = settings.errorClass;
-    this._inputErrorClass = settings.inputErrorClass;
-    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._settings = settings;
   }
 
-  enableValidation() {}
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formElement,
+        inputElement,
+        inputElement.validationMessage,
+        this._settings
+      );
+    } else {
+      hideInputError(this._formElement, inputElement, this._settings);
+    }
+  }
+
+  _toggleButtonState(inputList, buttonElement) {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add(this._settings.inactiveButtonClass);
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove(this._settings.inactiveButtonClass);
+      buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._settings.inputSelector)
+    );
+    const buttonElement = this._formElement.querySelector(
+      this._settings.submitButtonSelector
+    );
+
+    toggleButtonState(this._inputList, buttonElement, this._settings);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(this._inputList, buttonElement, this._settings);
+      });
+    });
+    console.log("Success");
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 
 export default FormValidator;
